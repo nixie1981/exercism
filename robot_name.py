6ceda948e4f9d0e4ca0024548ca598be130d1bee
nixie1981 <42901090+nixie1981@users.noreply.github.com>
2019-02-13 17:47:50 +1100

    Add files via upload

--- robot_name.py
+++ robot_name.py
@@ -1,19 +1,23 @@
 import random
 import string
 class Robot(object):
+    robot_names = []
+    @classmethod
+    def give_name(cls):
+        characters = []
+        for i in range(2):
+            characters.append(random.choice(string.ascii_uppercase))
+        for i in range(3):
+            characters.append(random.choice(string.digits))
+        name = ''.join(characters)
+        if name in cls.robot_names:
+            name = cls.give_name()
+        else:
+            cls.robot_names.append(name)
+        return name
     def __init__(self):
-        characters = []
-        for i in range(2):
-            characters.append(random.choice(string.ascii_uppercase))
-        for i in range(3):
-            characters.append(random.choice(string.digits))
-        self.name = ''.join(characters)
+        self.name = Robot.give_name()
     def reset(self):
-        characters = []
-        for i in range(2):
-            characters.append(random.choice(string.ascii_uppercase))
-        for i in range(3):
-            characters.append(random.choice(string.digits))
-        self.name = ''.join(characters)
+        self.name = Robot.give_name()
         
         
